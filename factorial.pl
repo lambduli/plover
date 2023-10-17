9140cbbec1a6737cd8d6c7ec2946b22c55a5710d
Jan <lambduli@gmail.com>
2023-10-17 15:26:47 +0200

    reorder the clauses such that Prolog wouldn't be able to deal with them

--- factorial.pl
+++ factorial.pl
@@ -1,6 +1,6 @@
-plus(z, N, N).
 plus(s(N), M, s(R)) :- plus(N, M, R).
-times(z, _, z).
+plus(z, N, N).
 times(s(N), M, A) :- times(N, M, R), plus(R, M, A).
-fact(z, s(z)).
+times(z, _, z).
 fact(s(N), R) :- fact(N, PR), times(s(N), PR, R).
+fact(z, s(z)).
